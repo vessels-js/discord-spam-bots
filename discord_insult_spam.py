432914339c6c1ad4a3c2b79f76f82ed8f1c611d9
Merubokkusu <Amagicalmailbox@gmail.com>
2017-08-01 13:10:10 -0400

    Update discord_insult_spam.py
    Tried to fix it....not able to test it, what could go wrong.

--- discord_insult_spam.py
+++ discord_insult_spam.py
@@ -4,7 +4,7 @@ import discord
 import asyncio
 from config import *
 
-url='http://www.insultgenerator.org/'
+url='http://foulomatic.hnldesign.nl/'
 
 
 client = discord.Client()
@@ -14,8 +14,8 @@ async def on_ready():
     while not client.is_closed:
         content = urllib.request.urlopen(url).read()
         soup = BeautifulSoup(content, "html.parser")
-        rows =soup.find_all('div',attrs={"class" : "wrap"})
-        for row in soup.find_all('div',attrs={"class" : "wrap"}):
+        rows =soup.find_all('div',attrs={"class" : "container"})
+        for row in soup.find_all('div',attrs={"class" : "well result"}):
                 print (row.text)
                 await client.send_message(discord.Object(id=DiscordChannel), row.text)
                 await asyncio.sleep(0.7) # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
