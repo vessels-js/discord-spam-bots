8230796a9cc5619ea51b2963426d67a69cc44b1b
Merubokkusu <Amagicalmailbox@gmail.com>
2017-08-03 21:36:17 -0400

    Update discord_insult_spam.py

--- discord_insult_spam.py
+++ discord_insult_spam.py
@@ -1,28 +1,16 @@
-import urllib.request
-from bs4 import BeautifulSoup, Comment
+import urllib
 import discord
 import asyncio
 from config import *
-
-url='http://foulomatic.hnldesign.nl/'
-insult_list = []
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    while not client.is_closed:
-        content = urllib.request.urlopen(url).read()
-        soup = BeautifulSoup(content, "html.parser")
-        rows =soup.find_all('div',attrs={"class" : "container"})
-        for row in soup.find_all('div',attrs={"class" : "well result"}):
-            insult_list.append(row.text) 
-                await client.send_message(discord.Object(id=DiscordChannel), insult_list[0] +" "+insult_list[1] +" "+ insult_list[3])
-                await asyncio.sleep(0.7) # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
-            
-            
-
+    insult_text = urllib.request.urlopen("https://insult.mattbas.org/api/insult.txt").read()
+    print(insult_text)
+    await client.send_message(discord.Object(id=DiscordChannel), insult_text)
+    await asyncio.sleep(0.7) # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
 
 print("Logged in ┬──┬ ノ( ゜-゜ノ)")
-
-client.run(userEmail, userPassword)
+client.run(userToken, bot=False)
